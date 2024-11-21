824a5ec7068f30b10c9b7e55f4e2aeafe025b766
RojeCodes <148307081+RojeCodes@users.noreply.github.com>
2024-11-21 15:24:20 +0800

    Create Booking.java

---
+++ src/main/java/classes/Booking.java
@@ -0,0 +1,60 @@
+package classes;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Booking {
+	private int booking_id;
+	private Date date;
+	private Time time;
+	private int hour_count;
+	private int service_id;
+	private int user_id;
+	private int receipt_id;
+	
+	public Booking (int booking_id, Date date, Time time,
+			int hour_count, int service_id, int user_id, int receipt_id) {
+		this.booking_id = booking_id;
+		this.date = date;
+		this.time = time;
+		this.hour_count = hour_count;
+		this.service_id = service_id;
+		this.user_id = user_id;
+		this.receipt_id = receipt_id;
+	}
+	
+	public int getBookingId() {
+		return this.booking_id;
+	}
+	public Date getDate() {
+		return this.date;
+	}
+	public Time getTime() {
+		return this.time;
+	}
+	public int getHourCount() {
+		return this.hour_count;
+	}
+	public int getServiceId() {
+		return this.service_id;
+	}
+	public int getUserId() {
+		return this.user_id;
+	}	
+	public int getReceiptId() {
+		return this.receipt_id;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	public void setHourCount(int hour_count) {
+		this.hour_count = hour_count;
+	}
+	public void setServiceId(int service_id) {
+		this.service_id = service_id;
+	}
+}
