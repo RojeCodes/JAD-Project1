9a4a3f5e7fca6e20d7fdc4ea28b92a500054ff91
kaungyemyintmo <148752406+kaungyemyintmo@users.noreply.github.com>
2024-11-24 07:31:08 +0800

    Update RegisterServlet.java

--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -18,6 +18,8 @@ import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import org.mindrot.bcrypt.BCrypt;
+
 // Import Database Connection Class file 
 import servlets.DatabaseConnection;
 
@@ -40,6 +42,57 @@ public class RegisterServlet extends HttpServlet {
 		System.out.print("ADD PRODUCT");
 	}
 
+	private boolean checkBanned(Connection conn, String email) {
+		boolean banned = false;
+		try {
+			String checkBannedStr = "SELECT * FROM \"user\" WHERE \"email\" = ? AND \"banned\" = ?";
+
+			PreparedStatement stmt = conn.prepareStatement(checkBannedStr);
+
+			stmt.setString(1, email);
+			stmt.setBoolean(2, true);
+
+			ResultSet bannedUser = stmt.executeQuery();
+
+			if (bannedUser.next()) {
+				banned = true;
+				System.out.println(banned);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return banned;
+	}
+
+	private boolean checkDuplicateEmail(Connection conn, String email) {
+		boolean duplicate = false;
+		try {
+			String duplicateEmailStr = "SELECT * FROM \"user\" WHERE \"email\" = ?";
+
+			PreparedStatement stmt = conn.prepareStatement(duplicateEmailStr);
+
+			stmt.setString(1, email);
+
+			ResultSet duplicateUser = stmt.executeQuery();
+
+			if (duplicateUser.next()) {
+				duplicate = true;
+				System.out.println(duplicate);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return duplicate;
+	}
+
+	// Use BCrypt to generate a hash
+	private String hashPassword(String password) {
+		String salt = BCrypt.gensalt(12);
+		return BCrypt.hashpw(password, BCrypt.gensalt());
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
@@ -48,6 +101,7 @@ public class RegisterServlet extends HttpServlet {
 		String paramName = "";
 		String[] paramValues = null;
 		ArrayList<String> databaseValues = new ArrayList<>();
+		Connection con = null;
 
 		// processing
 		String userEmail = request.getParameter("email");
@@ -61,100 +115,103 @@ public class RegisterServlet extends HttpServlet {
 			response.sendRedirect("client/register.jsp?errCode=passwordMismatch");
 			return;
 		}
-		
-		String birthdate = request.getParameter("birthdate"); 
+
+		String birthdate = request.getParameter("birthdate");
 		if (birthdate != null && !birthdate.isEmpty()) {
-		    try {
-		        LocalDate birthdateDate = LocalDate.parse(birthdate);
+			try {
+				LocalDate birthdateDate = LocalDate.parse(birthdate);
 
-		        // Calculate the date 18 years ago
-		        LocalDate eighteenYearsAgo = LocalDate.now().minusYears(18);
+				// Calculate the date 18 years ago
+				LocalDate eighteenYearsAgo = LocalDate.now().minusYears(18);
 
-		        if (birthdateDate.isBefore(eighteenYearsAgo) || birthdateDate.isEqual(eighteenYearsAgo)) {
-		            System.out.println("older than or exactly 18 years.");
-		        } else {
+				if (birthdateDate.isBefore(eighteenYearsAgo) || birthdateDate.isEqual(eighteenYearsAgo)) {
+					System.out.println("older than or exactly 18 years.");
+				} else {
 					response.sendRedirect("../client/register.jsp?errCode=ageRestricted");
 
-		            System.out.println("younger than 18 years.");
-		            return;
-		        }
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
-		}
-		
-		try {
-			// Initialize the database
-			Connection con = DatabaseConnection.initializeDatabase();
-
-			PreparedStatement selectStatement = con
-					.prepareStatement("select * from \"user\" where email = ? OR phone = ?");
-			selectStatement.setString(1, userEmail);
-			selectStatement.setString(2, userPhone);
-			System.out.println(userEmail);
-
-			ResultSet userSet = selectStatement.executeQuery();
-
-			// if duplicate user exist
-			if (userSet.next()) {
-//				System.out.println(userSet.getString(6));
-				response.sendRedirect("../client/register.jsp?errCode=duplicateEmail");
-				return;
-			} else {
-
-				PreparedStatement insertStatement = con.prepareStatement(
-						"insert into \"user\" (full_name, phone, address, birthdate, email, password) values(?, ?, ?, ?, ?, ?)");
-
-				if (params.hasMoreElements()) {
-
-					while (params.hasMoreElements()) {
-						// get parameter name to get parameter value accordingly
-						paramName = (String) params.nextElement();
-						// get parameter value
-						paramValues = request.getParameterValues(paramName);
-
-						System.out.println(paramName + ": " + request.getParameter(paramName));
-
-						// add to array in order
-						for (int i = 0; i < paramValues.length; i++) {
-							databaseValues.add(paramValues[i].toString());
-						}
-					}
-				} else {
-					System.out.println("No element in params");
-				}
-
-				// 6 columns in the table
-				if (databaseValues.size() >= 6) {
-					for (int i = 0, j = 1; i < 6; i++, j++) {
-						// getting datatype
-						System.out.println(databaseValues.get(i).getClass().getName());
-
-						if (i == 3) { // for birthdate
-							java.sql.Date sqlDate = java.sql.Date.valueOf(databaseValues.get(i));
-							insertStatement.setDate(j, sqlDate);
-						} else {
-							insertStatement.setString(j, databaseValues.get(i));
-						}
-					}
-				} else {
-					System.out.println("Insufficient parameters received.");
+					System.out.println("younger than 18 years.");
 					return;
 				}
-
-				// execute statement
-				insertStatement.executeUpdate();
-
-				// Close all the connections
-				selectStatement.close();
-				insertStatement.close();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+		}
+
+		try {
+			// Initialize the database
+			con = DatabaseConnection.initializeDatabase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (checkBanned(con, userEmail)) {
+			response.sendRedirect("../client/register.jsp?errCode=banned");
+			return;
+		}
+
+		if (checkDuplicateEmail(con, userEmail)) {
+			response.sendRedirect("../client/register.jsp?errCode=duplicateEmail");
+			return;
+		}
+
+		try {
+
+			PreparedStatement insertStatement = con.prepareStatement(
+					"insert into \"user\" (full_name, phone, address, birthdate, email, password) values(?, ?, ?, ?, ?, ?)");
+
+			if (params.hasMoreElements()) {
+
+				while (params.hasMoreElements()) {
+					// get parameter name to get parameter value accordingly
+					paramName = (String) params.nextElement();
+					// get parameter value
+					paramValues = request.getParameterValues(paramName);
+
+					System.out.println(paramName + ": " + request.getParameter(paramName));
+
+					// add to array in order
+					for (int i = 0; i < paramValues.length; i++) {
+						databaseValues.add(paramValues[i].toString());
+					}
+				}
+			} else {
+				System.out.println("No element in params");
+			}
+
+			// 6 columns in the table
+			if (databaseValues.size() >= 6) {
+				for (int i = 0, j = 1; i < 6; i++, j++) {
+					// getting datatype
+					System.out.println(databaseValues.get(i).getClass().getName());
+
+					if (i == 3) { // for birthdate
+						java.sql.Date sqlDate = java.sql.Date.valueOf(databaseValues.get(i));
+						insertStatement.setDate(j, sqlDate);
+					} else if (i == 5) {
+						String hashPwd = hashPassword(databaseValues.get(i));
+						insertStatement.setString(j, hashPwd);
+					} else {
+						insertStatement.setString(j, databaseValues.get(i));
+					}
+				}
+			} else {
+				System.out.println("Insufficient parameters received.");
+				return;
+			}
+
+			// execute statement
+			int insertedRow = insertStatement.executeUpdate();
+
+			if (insertedRow > 0) {
+				response.sendRedirect("../client/home.jsp?code=successfulRegistration");
+			}
+
+			// Close all the connections
+			insertStatement.close();
 			con.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		response.sendRedirect("../client/home.jsp");
 	}
 }
